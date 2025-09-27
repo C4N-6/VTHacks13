548902dd06ce7e41cc02f871bca10db724a93c86
Awab Eltigani <awabeltigani@Awabs-MacBook-Air.local>
2025-09-27 15:08:55 -0400

    function that returns a url

--- src/main.py
+++ src/main.py
@@ -5,3 +5,10 @@ if __name__ == "__main__":
     ai = AI()
 
     print(ai.askAI("hello how are you doing"))
+
+
+def getURL(filename):
+    if str(filename).count(".") > 1:
+        return "hidden files dont work"
+    name_without_extension = filename.rsplit(".", 1)[0]
+    return f"https://piazza.com/class/m63z5y05jgj3f6/post/{name_without_extension}"
