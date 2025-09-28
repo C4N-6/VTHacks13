54c9e1f85cc7557d6270f33dc8463e482823d862
C4N-6 <patetoooman@gmail.com>
2025-09-27 20:44:31 -0400

    made an api for the ai

--- src/main.py
+++ src/main.py
@@ -1,8 +1,20 @@
+from os import path
 from AI import AI
+from flask import Flask, jsonify, request
 
 
-if __name__ == "__main__":
-    ai = AI("questions-2114")
+app = Flask(__name__)
 
-    print(ai.askAI("how do I creat an array"))
+ai = AI("questions-2114")
 
+
+@app.route("/<s>")
+def mainPage(s: str):
+    result = ai.askAI(s)
+    return jsonify(result)
+
+
+# if __name__ == "__main__":
+#     ai = AI("questions-2114")
+#
+#     print(ai.askAI("how do I creat an array"))
